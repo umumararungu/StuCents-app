9beabdda3a92c3d1f928c5a89f9dc17f77a2fe04
umumararungu <umcynth@gmail.com>
2025-07-31 14:30:49 +0200

    deploy staging

--- student_expense_tracker/infra/staging/main.tf
+++ student_expense_tracker/infra/staging/main.tf
@@ -60,7 +60,7 @@ resource "azurerm_container_app_environment" "env" {
 
 resource "azurerm_container_app" "stucentsapp" {
   name                         = "stucents-staging-app"
-  container_app_environment_id = data.azurerm_container_app_environment.shared_env
+  container_app_environment_id = azurerm_container_app_environment.env.id
   resource_group_name          = azurerm_resource_group.rg.name
   revision_mode                = "Single"
 
